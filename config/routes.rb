8a01292b7701af11f0dad0e0e38533cd5e27faac
itouseiya <itouseiya12@gmail.com>
2023-10-24 12:37:39 +0900

    コメント投稿機能の実装に伴い、ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments, only:[:create, :destroy]
+  end
   devise_for :users
   root 'homes#top'
   get 'top' => 'homes#top'
