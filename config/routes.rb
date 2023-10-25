de36100eef56cc349e2520d67385e0751e48dfec
itouseiya <itouseiya12@gmail.com>
2023-10-25 14:47:16 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts do
     resources :comments, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   devise_for :users
   root 'homes#top'
