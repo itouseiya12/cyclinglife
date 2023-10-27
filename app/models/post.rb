85203ce6fe969fe5d5fa9a8dd1365eb0b40c04f8
itouseiya <itouseiya12@gmail.com>
2023-10-27 11:59:25 +0900

    投稿時に場所と説明文の入力を必須とする設定を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  validates :location, presence: true
+  validates :text, presence: true
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
