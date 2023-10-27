998371c4a85db02272d9593e00c8c5dce6aecd48
itouseiya <itouseiya12@gmail.com>
2023-10-27 10:50:18 +0900

    ユーザープロフィールの編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
-  resources :users, only:[:index, :show]
+  resources :users, only:[:index, :show, :edit, :update]
 end
