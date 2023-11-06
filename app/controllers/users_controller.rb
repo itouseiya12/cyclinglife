40686ec60cb01bd5670d77ff85f0ee5715e36c0e
itouseiya <itouseiya12@gmail.com>
2023-11-06 15:33:04 +0900

    アバター画像を設定できるように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :email, :profile)
+    params.require(:user).permit(:name, :email, :profile, :avatar)
   end
 end
