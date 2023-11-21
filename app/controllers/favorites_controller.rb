1963cb7b685538b9958ac43f978b23fef4d8e68d
itouseiya <itouseiya12@gmail.com>
2023-11-21 11:21:55 +0900

    いいね機能を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-      post = Post.find(params[:post_id])
-      favorite = current_user.favorites.new(post_id: post.id)
-      favorite.save
-      redirect_to post_path(post)
+    @favorite = current_user.favorites.create(post_id: params[:post_id])
+    redirect_back(fallback_location: root_path)
     end
     
     def destroy
-      post = Post.find(params[:post_id])
-      favorite = current_user.favorites.find_by(post_id: post.id)
-      favorite.destroy
-      redirect_to post_path(post)
+      @post = Post.find(params[:post_id])
+      @favorite = current_user.favorites.find_by(post_id: @post.id)
+      @favorite.destroy
+      redirect_back(fallback_location: root_path)
     end
 end
