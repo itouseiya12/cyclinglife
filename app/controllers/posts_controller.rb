69ce645ded1c9586f60c89318fe9fe7e988836f7
itouseiya <itouseiya12@gmail.com>
2023-12-07 14:20:22 +0900

    投稿内容・ユーザー情報更新時のフラッシュメッセージを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,8 +19,9 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-     redirect_to post_path(@post.id)
+     redirect_to post_path(@post.id), notice: "投稿が完了しました。"
     else
+      flash.now[:alert] = "投稿に失敗しました" 
      render :new, status: :unprocessable_entity
     end
   end
@@ -32,7 +33,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to post_path(@post.id)
+      redirect_to post_path(@post.id), notice: "投稿内容を更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,7 +42,7 @@ class PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to posts_path
+    redirect_to posts_path, notice: "投稿を削除しました。"
   end
   
   private
