75cb62dd38a2b85553fe1a5976143f5c25d98357
itouseiya <itouseiya12@gmail.com>
2023-12-14 17:23:15 +0900

    ユーザーがいいね・コメントした投稿の一覧ページへのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in' => 'users/sessions#guest_sign_in'
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :users, only:[:index, :show, :edit, :update]
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :favorites
+      get :comments
+    end
+  end
 end
